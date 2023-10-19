736a16fc06f849f3c7ee7e239cf5e085a2890d45
Alpha <alpha.likembe@student.moringaschool.com>
2023-10-19 03:19:04 +0300

    testing

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,26 +1,5 @@
 #!/usr/bin/env python3
 
-# class CashRegister:
-#   def __init__(self, total):
-#     self.total = total
-
-#   def apply_discount(self, discount_percentage):
-#     if discount_percentage < 0 or discount_percentage > 100:
-#       raise ValueError("Discount percentage must be between 0 and 100")
-    
-#     discount_amount = (discount_percentage / 100) * self.total
-#     self.total -= discount_amount
-
-#   def get_total(self):
-#     return self.total
-  
-# #Example
-# register = CashRegister(100)
-# print("Initial Total: $", register.get_total())
-# register.apply_discount(20)
-# print("After 20% Discount: $", register.get_total())
-
-
 class CashRegister:
     def __init__(self, discount = 0):
        self.discount = discount
